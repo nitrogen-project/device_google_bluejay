427c905ad7f67fcaf559e68fd98edeb33789cc79
xyyx <xyyx@mail.ru>
2022-11-24 13:52:27 +0300

    Nitrogen
    Change-Id: I8061f809f0038f159fd2d8ee34cc5a8954d3c505

---
+++ nitrogen_bluejay.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2021 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/google/gs101/aosp_common.mk)
+$(call inherit-product, device/google/bluejay/device-bluejay.mk)
+
+# Inherit some
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
+
+# Gapps
+$(call inherit-product, vendor/google/gms/config.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := nitrogen_bluejay
+PRODUCT_DEVICE := bluejay
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel 6a
+PRODUCT_MANUFACTURER := Google
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bluejay \
+    PRIVATE_BUILD_DESC="bluejay-user 14 UP1A.231005.007 10754064 release-keys"
+
+BUILD_FINGERPRINT := google/bluejay/bluejay:14/UP1A.231005.007/10754064:user/release-keys
